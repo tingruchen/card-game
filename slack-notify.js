2834daf2f8cee81ef1c9aaff17df6d79227f141c
Tiffany Chen <tiffany.chen@104.com.tw>
2023-01-09 16:49:19 +0800

    test notify

--- slack-notify.js
+++ slack-notify.js
@@ -1,19 +1,15 @@
 const axios = require("axios").default;
 
-async function notify({ CIRCLE_TAG, CIRCLE_BUILD_URL }) {
+async function notify({ CIRCLE_TAG, CIRCLE_BUILD_URL, SLACK_NOTIFY_URL }) {
   const text = CIRCLE_TAG
     ? `CircleCI build completed. <${CIRCLE_BUILD_URL}|See build>\n\`${CIRCLE_TAG}\` released. \nReleased at: https://github.com/tingruchen/card-game/releases/tag/${CIRCLE_TAG}`
     : `CircleCI build completed. <${CIRCLE_BUILD_URL}|See build>\nApp deployed at: https://card-game-97514.web.app`;
   try {
-    await axios.post(
-      "https://hooks.slack.com/services/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",
-      JSON.stringify({ text }),
-      {
-        headers: {
-          "Content-type": "application/json"
-        }
+    await axios.post(SLACK_NOTIFY_URL, JSON.stringify({ text }), {
+      headers: {
+        "Content-type": "application/json"
       }
-    );
+    });
   } catch (err) {
     console.error("notify failed. ", err);
   }
