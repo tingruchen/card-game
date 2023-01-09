7a4d31f6c8858a0efca0877d43383fada87b10dd
Hsin-I Tiffany Chen <55262011+ItsTiffanyChen@users.noreply.github.com>
2023-01-09 17:10:51 +0800

    test notify (#16)
    Co-authored-by: Tiffany Chen <tiffany.chen@104.com.tw>

--- slack-notify.js
+++ slack-notify.js
@@ -2,7 +2,7 @@ const axios = require("axios").default;
 
 async function notify({ CIRCLE_TAG, CIRCLE_BUILD_URL, SLACK_NOTIFY_URL }) {
   const text = CIRCLE_TAG
-    ? `CircleCI build completed. <${CIRCLE_BUILD_URL}|See build>\n\`${CIRCLE_TAG}\` released. \nReleased at: https://github.com/tingruchen/card-game/releases/tag/${CIRCLE_TAG}`
+    ? `CircleCI build completed. <${CIRCLE_BUILD_URL}|See build>\n\`${CIRCLE_TAG}\` released at: https://github.com/tingruchen/card-game/releases/tag/${CIRCLE_TAG}`
     : `CircleCI build completed. <${CIRCLE_BUILD_URL}|See build>\nApp deployed at: https://card-game-97514.web.app`;
   try {
     await axios.post(SLACK_NOTIFY_URL, JSON.stringify({ text }), {
