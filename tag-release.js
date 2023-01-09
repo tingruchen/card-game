9b3d3fcd950ddd02fc879f659549c7b1b2ac2efa
Tiffany Chen <tiffany.chen@104.com.tw>
2023-01-09 13:12:06 +0800

    test release

--- tag-release.js
+++ tag-release.js
@@ -1,10 +1,11 @@
 const zipper = require("zip-local");
 const axios = require("axios").default;
 
-async function performRelease({ GITHUB_TOKEN, CIRCLE_TAG = "0.0.4" }) {
-  let uploadUrl;
+async function performRelease({ GITHUB_TOKEN, CIRCLE_TAG = "0.0.6" }) {
   try {
-    let { upload_url } = await axios.post(
+    let {
+      data: { upload_url: uploadUrl }
+    } = await axios.post(
       "https://api.github.com/repos/tingruchen/card-game/releases",
       {
         tag_name: CIRCLE_TAG,
@@ -13,16 +14,39 @@ async function performRelease({ GITHUB_TOKEN, CIRCLE_TAG = "0.0.4" }) {
         body: `release of ${CIRCLE_TAG}`,
         draft: false,
         prerelease: false,
-        generate_release_notes: false,
+        generate_release_notes: false
       },
       {
         headers: {
           Accept: "application/vnd.github+json",
-          Authorization: `token ${GITHUB_TOKEN}`,
-        },
+          Authorization: `token ${GITHUB_TOKEN}`
+        }
       }
     );
-    uploadUrl = upload_url.split("{?name,label}")[0];
+
+    zipper.zip("./dist", async function (error, zipped) {
+      if (!error) {
+        zipped.compress(); // compress before exporting
+
+        var buff = zipped.memory(); // get the zipped file as a Buffer
+
+        try {
+          await axios.post(
+            `${uploadUrl.split("{?name,label}")[0]}?name=dist.zip`,
+            buff,
+            {
+              headers: {
+                Accept: "application/vnd.github+json",
+                "Content-Type": "application/zip",
+                Authorization: `token ${GITHUB_TOKEN}`
+              }
+            }
+          );
+        } catch (err) {
+          console.error("assets err", err);
+        }
+      }
+    });
   } catch (err) {
     console.error("release err", err);
   }
@@ -45,26 +69,6 @@ async function performRelease({ GITHUB_TOKEN, CIRCLE_TAG = "0.0.4" }) {
   // } catch (err) {
   //   console.error("errrrr", err);
   // }
-
-  zipper.zip("./dist", async function (error, zipped) {
-    if (!error) {
-      zipped.compress(); // compress before exporting
-
-      var buff = zipped.memory(); // get the zipped file as a Buffer
-
-      try {
-        await axios.post(`${uploadUrl}?name=dist.zip`, buff, {
-          headers: {
-            Accept: "application/vnd.github+json",
-            "Content-Type": "application/zip",
-            Authorization: `token ${GITHUB_TOKEN}`,
-          },
-        });
-      } catch (err) {
-        console.error("assets err", err);
-      }
-    }
-  });
 }
 
 performRelease(process.env);
