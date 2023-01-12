dfd3451029926be1138b2e7360291e4d661492bf
ShineCheng <afireflytw@gmail.com>
2023-01-12 16:29:39 +0800

    測試失敗

--- src/test/HelloWorld.test.js
+++ src/test/HelloWorld.test.js
@@ -11,6 +11,6 @@ describe("HelloWorld", () => {
   });
 
   test("msg should be HelloWorld", () => {
-    expect(wrapper.find(`[data-e2e="msg"]`).text()).to.equal(msg);
+    expect(wrapper.find(`[data-e2e="fail"]`).text()).to.equal(msg);
   });
 });
